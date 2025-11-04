8b20aa6e8a212a2b3a7b9079cfa43105bf745112
Matias-Segovia-fullstack <mati.segovia@duocuc.cl>
2025-11-04 13:32:25 -0300

    Add files via upload
    Base de datos subida

---
+++ Base_Datos/src/test/java/Backend/Base_Datos/BaseDatosApplicationTests.java
@@ -0,0 +1,13 @@
+package Backend.Base_Datos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
