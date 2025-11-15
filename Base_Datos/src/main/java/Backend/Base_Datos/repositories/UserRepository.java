ed363746f99de50b3900f31ca562fb6b56d0c180
Matias <mati.segovia@duocuc.cl>
2025-11-15 01:48:20 -0300

    repository de usuario

---
+++ Base_Datos/src/main/java/Backend/Base_Datos/repositories/UserRepository.java
@@ -0,0 +1,24 @@
+package Backend.Base_Datos.repositories;
+
+import Backend.Base_Datos.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+  Optional<User> findByRut(String rut);
+
+  Optional<User> findByCorreo(String correo);
+
+  Optional<User> findByContrasena(String contrasena);
+
+  Optional<User> findByCorreoAndContrasena(String correo, String contrasena);
+
+  List<User> findAll();
+
+  long count();
+}
