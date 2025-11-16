1ce09efb196a7c2e604e0ef47ae58a885a0dd89e
Matias <mati.segovia@duocuc.cl>
2025-11-16 03:50:33 -0300

    model de producto

---
+++ Base_Datos/src/main/java/Backend/Base_Datos/models/Product.java
@@ -0,0 +1,37 @@
+package Backend.Base_Datos.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Entity
+@Table(name="producto")
+@Getter @Setter @ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idProducto;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank(message = "El nombre es obligatorio")
+    private String nombreProducto;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Debe ingresar Categoria")
+    private String categoria;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Debe ingresar Precio")
+    private String price;
+
+    @Column(nullable = false)
+    @NotBlank(message = "El stock no puede ser vacio")
+    private String stock;
+
+    @Column
+    private String imageUrl;
+
+}
